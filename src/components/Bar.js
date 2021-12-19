76a9fa754cd3eaaa1917c39909afcdb5fef38c1e
pmozil <mozil.petryk@gmail.com>
2021-12-19 16:52:41 +0200

    Add miniscule mobile support

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,4 +1,13 @@
 import { Link } from "react-router-dom";
+import {FaPlus,
+		FaComments,
+		FaBook,
+		FaScroll,
+		FaLeanpub,
+		FaInfo,
+		FaInfoCircle,
+		FaGraduationCap
+} from "react-icons/fa";
 import '../scss/App.scss';
 import '../scss/Sidebar.scss';
 
@@ -8,43 +17,67 @@ function Menu(props) {
 			<nav className="top-bar">
 				<ul className="links">
             	    <li>
-							<Link className="menu-text" to="/about">
-									About
+							<Link to="/about">
+								<div className="li-container">
+								<p className="text">About</p>
+								<FaPlus className="icon"/>
+								</div>
 							</Link>
             	    </li>
             	    <li>
-							<Link className="menu-text" to="/community">
-									Community
+							<Link to="/community">
+								<div className="li-container">
+								<p className="text">Community</p>
+								<FaComments className="icon"/>
+								</div>
             	        	</Link>
             	    </li>
             	    <li>
-		    	             <Link className="menu-text" to="/education">
-									Education quality
+		    	             <Link to="/education">
+								<div className="li-container">
+								<p className="text">Education quality</p>
+								<FaBook className="icon"/>
+								</div>
    		    	             </Link>
             	    </li>
             	    <li>
-		    	             <Link className="menu-text" to="/for-students">
-									For students
+		    	             <Link to="/for-students">
+								<div className="li-container">
+								<p className="text">For students</p>
+								<FaScroll className="icon"/>
+								</div>
    		    	             </Link>
             	    </li>
             	    <li>
-		    	             <Link className="menu-text" to="/for-entrants">
-									For entrants
+		    	             <Link to="/for-entrants">
+								<div className="li-container">
+								<p className="text">For entrants</p>
+								<FaLeanpub className="icon"/>
+								</div>
    		    	             </Link>
             	    </li>
             	    <li>
-		    	             <Link className="menu-text" to="/for-parents">
-									For parents
+		    	             <Link to="/for-parents">
+								<div className="li-container">
+								<p className="text">For parents</p>
+								<FaInfo className="icon"/>
+								</div>
    		    	             </Link>
             	    </li>
             	    <li>
-		    	             <Link className="menu-text" to="/public-info">
-									Public info
+		    	             <Link to="/public-info">
+								<div className="li-container">
+								<p className="text">Public info</p>
+								<FaInfoCircle className="icon"/>
+								</div>
    		    	             </Link>
             	    </li>
             	    <li>
-		    	             <Link className="menu-text" to="/for-grads">
-									For graduates
+		    	             <Link to="/for-grads">
+								<div className="li-container">
+								<p className="text">For graduates</p>
+								<FaGraduationCap className="icon"/>
+								</div>
    		    	             </Link>
             	    </li>
 					<li className="bottom">
@@ -55,7 +88,7 @@ function Menu(props) {
         					    >
         					        {props.text}
         					</button>
-							<Link className="menu-text" to="/" className="bottom">
+							<Link to="/" className="bottom">
 									Gimme the god forsaken svg already (home)
         					</Link>
 						</div>
