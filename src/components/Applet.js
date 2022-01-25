acef0a988117b4c311605436cd81cf06342430dd
rizerphe <44440399+BohdanOpyr@users.noreply.github.com>
2022-01-25 01:00:15 +0000

    Hardcoded grid positions in an different place

--- src/components/Applet.js
+++ src/components/Applet.js
@@ -2,7 +2,7 @@ import "../scss/App.scss";
 import { Link } from "react-router-dom";
 
 // TODO: Add text from article instead of hello,
-// add imege rendering from article instead of picsum
+// add image rendering from articles instead of picsum
 
 function ArticleLink(props) {
     return (
@@ -28,77 +28,80 @@ function ArticleLink(props) {
 }
 
 function Applet(props) {
+    // This will be an API call eventually
+    let articles = [];
+    for(var i = 0;i<11;i++) {
+        articles.push(
+            {
+                to: `news/${i}`,
+                cover: i < 8 ? "https://picsum.photos/300" : "https://picsum.photos/800"
+            }
+        );
+    };
+
+    // These are roughly sorted from the most to the least eye-catching tile
+    // Only the first few tiles will be visible on mobile
+
+    // I've given up on generating this procedurally for a given screen size
+    // (5 hours wasted)
+    // leaving it all hard-coded for now
+    const positions = [
+        {
+            row: "1 / 16",
+            column: "2 / 4"
+        },
+        {
+            row: "1 / 12",
+            column: "1 / 2"
+        },
+        {
+            row: "1 / 12",
+            column: "4 / 6"
+        },
+        {
+            row: "12 / 24",
+            column: "4 / 6"
+        },
+        {
+            row: "16 / 24",
+            column: "2 / 4"
+        },
+        {
+            row: "1 / 16",
+            column: "6 / 7"
+        },
+        {
+            row: "12 / 24",
+            column: "1 / 2"
+        },
+        {
+            row: "16 / 24",
+            column: "6 / 7"
+        },
+        {
+            row: "24 / 48",
+            column: "1 / 3"
+        },
+        {
+            row: "24 / 48",
+            column: "3 / 5"
+        },
+        {
+            row: "24 / 48",
+            column: "5 / 7"
+        }
+    ];
+
     return (
         <div className="Applet">
-            {/* Top tow */}
-            <ArticleLink
-                to="new/1"
-                row="1 / 12"
-                column="1 / 2"
-                img="https://picsum.photos/300"
-            />
-            <ArticleLink
-                to="new/2"
-                row="1 / 16"
-                column="2 / 4"
-                img="https://picsum.photos/300"
-            />
-            <ArticleLink
-                to="new/3"
-                row="1 / 12"
-                column="4 / 6"
-                img="https://picsum.photos/300"
-            />
-            <ArticleLink
-                to="new/4"
-                row="1 / 16"
-                column="6 / 7"
-                img="https://picsum.photos/300"
-            />
-            {/* Middle row */}
-            <ArticleLink
-                to="new/5"
-                row="12 / 24"
-                column="1 / 2"
-                img="https://picsum.photos/300"
-            />
-            <ArticleLink
-                to="new/6"
-                row="16 / 24"
-                column="2 / 4"
-                img="https://picsum.photos/300"
-            />
-            <ArticleLink
-                to="new/7"
-                row="12 / 24"
-                column="4 / 6"
-                img="https://picsum.photos/300"
-            />
-            <ArticleLink
-                to="new/8"
-                row="16 / 24"
-                column="6 / 7"
-                img="https://picsum.photos/300"
-            />
-            {/* Bottom row */}
-            <ArticleLink
-                to="new/9"
-                row="24 / 48"
-                column="1 / 3"
-                img="https://picsum.photos/800"
-            />
-            <ArticleLink
-                to="new/10"
-                row="24 / 48"
-                column="3 / 5"
-                img="https://picsum.photos/800"
-            />
-            <ArticleLink
-                to="new/11"
-                row="24 / 48"
-                column="5 / 7"
-                img="https://picsum.photos/800"
-            />
+            {articles.map((article, index) => (
+                <ArticleLink
+                    to={article.to}
+                    row={positions[index].row}
+                    column={positions[index].column}
+                    img={article.cover}
+                />
+            ))}
         </div>
     );
 }
