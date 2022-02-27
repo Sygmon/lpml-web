5cc94d7e9d6f28da570b5f4f0567df1e0d8d2039
Petro Mozil <mozil.petryk@gmail.com>
2022-02-27 13:37:35 +0200

    Move scss and components out of pages folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import "./scss/App.scss";
-import App from "./components/App.js";
+import "../index.scss";
+import "../scss/App.scss";
+import App from "../components/App.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export default function IndexPage() {
