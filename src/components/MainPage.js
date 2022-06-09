99c6710274fdf94eca99a351ba3445e6012072e7
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-06-09 14:24:25 +0300

    Fix a couple warnings

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,33 +6,33 @@ export default function MainPage({}) {
     return (
         <>
             <div className={styles.main}>
-                <span class={styles.heading}>
+                <span className={styles.heading}>
                     Lviv Physics and Maths Liceum
                 </span>
-                <div class={styles.corner_nav}>
-                    <span class={styles.nav_button}>
+                <div className={styles.corner_nav}>
+                    <span className={styles.nav_button}>
                         <Link href="/info/about">
-                            <span class={styles.nav_button_text}>
+                            <span className={styles.nav_button_text}>
                                 About
                             </span>
                         </Link>
                     </span>
-                    <span class={styles.nav_button}>
+                    <span className={styles.nav_button}>
                         <Link href="/info/for-entrants">
-                            <span class={styles.nav_button_text}>
+                            <span className={styles.nav_button_text}>
                                 For entrants
                             </span>
                         </Link>
                     </span>
-                    <span class={styles.nav_button}>
+                    <span className={styles.nav_button}>
                         <Link href="#articles_grid_applet" scroll={true}>
-                            <span class={styles.nav_button_text}>
+                            <span className={styles.nav_button_text}>
                                 News
                             </span>
                         </Link>
                     </span>
                 </div>
-                <div class={styles.logo}>
+                <div className={styles.logo}>
                     <img src="/logo.svg" alt="LPML logo" />
                 </div>
             </div>
