b57e77b6f55cb7946a6e6ad3ff1279a163c9f0bc
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-09-05 18:48:31 +0300

    Added for graduates subdirectory handling

---
+++ src/pages/info/for-grads/index.tsx
@@ -0,0 +1,29 @@
+import { GetStaticProps } from "next";
+import { getArticles } from "../../../lib/articles";
+import Article from "../../../components/Article";
+import Head from 'next/head'
+
+export default function ArticlePage({ id, content, title, articles }) {
+    return (
+      <>
+        <Article id={id} content={content} />
+        <Head>
+          <title>LPML - {title}</title>
+        </Head>
+      </>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const articles = getArticles("info");
+  const article = articles.filter(article => article.id === "for-grads")[0];
+  const eq_articles = getArticles("info/for-grads");
+  return {
+    props: {
+        id: article.id,
+        content: article.content,
+        title: article.title,
+        articles: eq_articles.map(article => ({id: article.id, title: article.title}))
+    },
+  };
+};
