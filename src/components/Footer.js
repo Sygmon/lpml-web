2de55ce1937af610b8417c20c60cf4b2f7b6227b
rhusiev <h.radomyr@proton.me>
2023-08-07 14:22:44 +0300

    Make the facebook icon in footer clickable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,42 +14,37 @@ function FooterLink({ href, children }) {
     );
 }
 
-export default function Footer () {
+export default function Footer() {
     return (
         <div className={styles.footer}>
             <div className={styles.column}>
                 <div className={styles.links}>
-                    <FooterLink href="/info/about">
-                        ліцей
-                    </FooterLink>
-                    <FooterLink href="/info/for-entrants">
-                        вступ
-                    </FooterLink>
+                    <FooterLink href="/info/about">ліцей</FooterLink>
+                    <FooterLink href="/info/for-entrants">вступ</FooterLink>
                     <FooterLink href="/info/for-students">
                         для ліцеїстів
                     </FooterLink>
-                    <FooterLink href="/info/public-info">
-                        публічно
-                    </FooterLink>
+                    <FooterLink href="/info/public-info">публічно</FooterLink>
                 </div>
             </div>
             <div className={styles.column}>
-                <div className={styles.label}>
-                    Контактна інформація:
-                </div>
+                <div className={styles.label}>Контактна інформація:</div>
                 <div className={styles.label}>
                     79059, Україна, м. Львів, вул. Караджича, 29
                 </div>
-                <div className={styles.label}>
-                    телефон: (032) 262-00-68
-                </div>
+                <div className={styles.label}>телефон: (032) 262-00-68</div>
             </div>
             <div className={styles.column}>
-                <div className={styles.label}>
-                    Ми у соцмережах
-                </div>
+                <div className={styles.label}>Ми у соцмережах</div>
                 <div className={styles.socials}>
-                    <FaFacebook />
+                    <Link href="http://www.facebook.com/groups/33427370676/">
+                        <a
+                            href="http://www.facebook.com/groups/33427370676/"
+                            className={styles.social}
+                        >
+                            <FaFacebook />
+                        </a>
+                    </Link>
                 </div>
                 <Link href="/">
                     <a href="/" className={styles.logo}>
