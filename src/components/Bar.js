84e3a8672a6e43b076aebecd6eecc4fa2b5efed6
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-08-16 10:19:23 +0300

    Add main page mobile navlink

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -6,131 +6,137 @@ import Link from "next/link";
 import SearchBar from "./Search";
 import { RiSearch2Line } from "react-icons/ri";
 
-function MenuItem({ hide, href, children }) {
-    return (
-        <Link href={href}>
-            <a href={href} className={styles.navlink} onClick={hide}>
-                {children}
-            </a>
-        </Link>
-    );
+function MenuItem({ hide, href, children, mobile = false }) {
+  return (
+    <Link href={href}>
+      <a
+        href={href}
+        className={`${styles.navlink} ${
+          mobile ? styles["navlink-mobile"] : ""
+        }`}
+        onClick={hide}
+      >
+        {children}
+      </a>
+    </Link>
+  );
 }
 
 function MenuDropdown({ title, children }) {
-    const [open, setOpen] = useState(false);
-    return (
-        <div className={styles.dropdown}>
-            <button
-                className={styles.dropdownbutton}
-                onClick={() => setOpen(!open)}
-            >
-                {title}
-                <svg
-                    className={`${open ? styles.open : styles.closed} ${
-                        styles.dropdownicon
-                    }`}
-                    viewBox="0 0 24 24"
-                >
-                    <path
-                        d="M7.41 8.59L12 13.17L16.59 8.59L18 10L12 16L6 10L7.41 8.59Z"
-                        fill="currentColor"
-                    />
-                </svg>
-            </button>
-            <div
-                className={`${styles.dropdowncontent} ${
-                    open ? styles.open : styles.closed
-                }`}
-            >
-                {children}
-            </div>
-        </div>
-    );
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={styles.dropdown}>
+      <button className={styles.dropdownbutton} onClick={() => setOpen(!open)}>
+        {title}
+        <svg
+          className={`${open ? styles.open : styles.closed} ${
+            styles.dropdownicon
+          }`}
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M7.41 8.59L12 13.17L16.59 8.59L18 10L12 16L6 10L7.41 8.59Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+      <div
+        className={`${styles.dropdowncontent} ${
+          open ? styles.open : styles.closed
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
 
 function DrawerButton() {
-    return (
-        <label htmlFor="drawer" className={styles.drawerlabel}>
-            <FaBars className={`${styles.icon} ${styles.menubutton}`} />
-        </label>
-    );
+  return (
+    <label htmlFor="drawer" className={styles.drawerlabel}>
+      <FaBars className={`${styles.icon} ${styles.menubutton}`} />
+    </label>
+  );
 }
 
 export { SearchBar, MenuItem, DrawerButton };
 
 export default function Topbar() {
-    const checkbox = useRef(null);
-    const [searchOpen, setSearchOpen] = useState(false);
+  const checkbox = useRef(null);
+  const [searchOpen, setSearchOpen] = useState(false);
 
-    const hide = () => {
-        setSearchOpen(false);
-        checkbox.current.checked = false;
-    };
-    const toggleSearch = () => {
-        // If screen size < 1000px, do nothing
-        if (window.innerWidth < 1000) return;
-        setSearchOpen(!searchOpen);
-    };
-    return (
-        <>
-            <input
-                className={styles.drawer}
-                id="drawer"
-                type="checkbox"
-                ref={checkbox}
-            />
-            <div className={styles.titlebar}>
-                <DrawerButton />
-                <Link href="/">
-                    <a href="/" className={styles.name} onClick={hide}>
-                        ЛФМЛ
-                    </a>
-                </Link>
-                <span
-                    className={`${styles.search} ${
-                        searchOpen ? styles.open : styles.closed
-                    }`}
-                >
-                    <RiSearch2Line
-                        onClick={() => toggleSearch()}
-                        className={styles.searchbutton}
-                    />
-                    <div className={searchOpen ? styles.open : styles.closed}>
-                        <SearchBar hide={hide} />
-                    </div>
-                </span>
-                <MenuItem hide={hide} href="/news">
-                    новини
-                </MenuItem>
-                <MenuItem hide={hide} href="/info/for-entrants">
-                    вступ
-                </MenuItem>
-                <MenuDropdown title="ліцей">
-                    <MenuItem hide={hide} href="/info/about">
-                        про ліцей
-                    </MenuItem>
-                    <MenuItem hide={hide} href="/info/education">
-                        якість
-                    </MenuItem>
-                    <MenuItem hide={hide} href="/info/staff">
-                        колектив
-                    </MenuItem>
-                </MenuDropdown>
-                <MenuDropdown title="інформація">
-                    <MenuItem hide={hide} href="/info/public-info">
-                        публічно
-                    </MenuItem>
-                    <MenuItem hide={hide} href="/info/for-parents">
-                        батькам
-                    </MenuItem>
-                    <MenuItem hide={hide} href="/info/for-students">
-                        ліцеїстам
-                    </MenuItem>
-                    <MenuItem hide={hide} href="/info/for-grads">
-                        випускникам
-                    </MenuItem>
-                </MenuDropdown>
-            </div>
-        </>
-    );
+  const hide = () => {
+    setSearchOpen(false);
+    checkbox.current.checked = false;
+  };
+  const toggleSearch = () => {
+    // If screen size < 1000px, do nothing
+    if (window.innerWidth < 1000) return;
+    setSearchOpen(!searchOpen);
+  };
+  return (
+    <>
+      <input
+        className={styles.drawer}
+        id="drawer"
+        type="checkbox"
+        ref={checkbox}
+      />
+      <div className={styles.titlebar}>
+        <DrawerButton />
+        <Link href="/">
+          <a href="/" className={styles.name} onClick={hide}>
+            ЛФМЛ
+          </a>
+        </Link>
+        <span
+          className={`${styles.search} ${
+            searchOpen ? styles.open : styles.closed
+          }`}
+        >
+          <RiSearch2Line
+            onClick={() => toggleSearch()}
+            className={styles.searchbutton}
+          />
+          <div className={searchOpen ? styles.open : styles.closed}>
+            <SearchBar hide={hide} />
+          </div>
+        </span>
+        <MenuItem hide={hide} href="/" mobile>
+          головна
+        </MenuItem>
+        <MenuItem hide={hide} href="/news">
+          новини
+        </MenuItem>
+        <MenuItem hide={hide} href="/info/for-entrants">
+          вступ
+        </MenuItem>
+        <MenuDropdown title="ліцей">
+          <MenuItem hide={hide} href="/info/about">
+            про ліцей
+          </MenuItem>
+          <MenuItem hide={hide} href="/info/education">
+            якість
+          </MenuItem>
+          <MenuItem hide={hide} href="/info/staff">
+            колектив
+          </MenuItem>
+        </MenuDropdown>
+        <MenuDropdown title="інформація">
+          <MenuItem hide={hide} href="/info/public-info">
+            публічно
+          </MenuItem>
+          <MenuItem hide={hide} href="/info/for-parents">
+            батькам
+          </MenuItem>
+          <MenuItem hide={hide} href="/info/for-students">
+            ліцеїстам
+          </MenuItem>
+          <MenuItem hide={hide} href="/info/for-grads">
+            випускникам
+          </MenuItem>
+        </MenuDropdown>
+      </div>
+    </>
+  );
 }
