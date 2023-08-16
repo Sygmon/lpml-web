ba922fb1d92b743cbe6e691c86a8909e2c9302ed
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-08-16 11:37:17 +0300

    Improve accessibility

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
-    
-        return config;
-    }
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  i18n: {
+    locales: ["uk"],
+    defaultLocale: "uk",
+  },
 };
+
