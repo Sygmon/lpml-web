492d00dd9c2924ce9051237d30a814d5a5a88292
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-08-17 20:12:15 +0300

    Update error pages

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -7,14 +7,14 @@ export default function App({}) {
   return (
     <>
       <Head>
-        <title>404 - Львівський фізико-математичний ліцей</title>
-        <meta name="description" content="404 - Сторінка не знайдена" />
+        <title>500 - Львівський фізико-математичний ліцей</title>
+        <meta name="description" content="500 - Помилка сервера" />
       </Head>
       <div className={styles.container}>
         <div className={styles.error}>
-          <h1 className={styles.title}>404</h1>
+          <h1 className={styles.title}>500</h1>
           <div className={styles.text}>
-            <p className={styles.description}>Сторінка не знайдена</p>
+            <p className={styles.description}>Помилка сервера</p>
             <Link href="/">Повернутися на головну</Link>
           </div>
         </div>
