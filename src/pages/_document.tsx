4ad41035a259285d11d004e2f65f9075ffe63b6b
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-08-30 10:59:15 +0300

    Fix netlify identity widget

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="uk">
       <Head>
-        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         <meta
           name="keywords"
           content="львівський фізико математичний ліцей хіміко біологічний школа інтернат інформатика фізика математика хімія біологія"
         />
+        <script
+          async
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+        />
       </Head>
       <body>
-        <Main />
         <NextScript />
-        <div data-netlify-identity-menu></div>
+        <Main />
       </body>
     </Html>
   );
