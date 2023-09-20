2bd5d1b11d4406fc70679ec5bfe379f644c42324
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-09-20 23:27:27 +0300

    Fix main page alignment

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -5,30 +5,28 @@ import Logo from "./logo.svg";
 
 export default function MainPage() {
   return (
-    <div className={styles.content}>
-      <div className={styles.info}>
-        <div className={styles.container}>
-          <div className={styles.logo}>
-            <Logo />
-          </div>
-          <div className={styles.text}>
-            Загальноосвітній навчальний заклад із поглибленим вивченням
-            природничих дисциплін, який дає змогу здобути освіту понад державний
-            освітній мінімум, здійснює науково-практичну підготовку здібної,
-            талановитої учнівської молоді.
-          </div>
-          <span className={styles.separator} />
-          <div className={styles.text}>
-            Навчання в ліцеї відбувається українською мовою.
-          </div>
+    <div className={styles.info}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div className={styles.about}>
+        <div className={styles.text}>
+          Загальноосвітній навчальний заклад із поглибленим вивченням
+          природничих дисциплін, який дає змогу здобути освіту понад державний
+          освітній мінімум, здійснює науково-практичну підготовку здібної,
+          талановитої учнівської молоді.
+        </div>
+        <span className={styles.separator} />
+        <div className={styles.text}>
+          Навчання в ліцеї відбувається українською мовою.
         </div>
       </div>
-      <div className={styles.intro}>
-        <span className={styles.title}>
-          Львівський
-          <br />
-          фізико-математичний ліцей
-        </span>
+      <span className={styles.title}>
+        Львівський
+        <br />
+        фізико-математичний ліцей
+      </span>
+      <div className={styles.cover}>
         <Link href="/info/about">
           <div className={styles.image}>
             <span className={styles.overlay} />
