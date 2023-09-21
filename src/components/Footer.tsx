703287eb1548a1f33e28bb775721e394126b30bf
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-09-21 07:43:05 +0300

    Fix footer spacing and empty space below footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,49 +32,57 @@ export default function Footer() {
         </div>
       </div>
       <div className={styles.column}>
-        <div className={styles.label}>Контактна інформація:</div>
-        <div className={styles.label}>
-          79059, Україна, м. Львів, вул. Караджича, 29
+        <div className={styles.group}>
+          <div className={styles.label}>Контактна інформація:</div>
+          <div className={styles.label}>
+            79059, Україна, м. Львів, вул. Караджича, 29
+          </div>
+          <div className={styles.label}>телефон: (032) 262-00-68</div>
         </div>
-        <div className={styles.label}>телефон: (032) 262-00-68</div>
       </div>
       <div className={styles.column}>
-        <div className={styles.label}>Ми у соцмережах</div>
-        <div className={styles.logos}>
-          <Link href="http://www.facebook.com/groups/33427370676/">
-            <a
-              href="http://www.facebook.com/groups/33427370676/"
-              className={styles.social}
-              aria-label="facebook"
-            >
-              <FaFacebook />
+        <div className={styles.group}>
+          <div className={styles.label}>Ми у соцмережах</div>
+          <div className={styles.logos}>
+            <Link href="http://www.facebook.com/groups/33427370676/">
+              <a
+                href="http://www.facebook.com/groups/33427370676/"
+                className={styles.social}
+                aria-label="facebook"
+              >
+                <FaFacebook />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <Link href="/info/about/конкурси">
+            <div className={`${styles.label} ${styles.link}`}>Конкурси</div>
+          </Link>
+          <div className={styles.logos}>
+            <a href="http://kangaroo.com.ua">
+              <img src="/images/contests/kangaroo.png" alt="kangaroo" />
+            </a>
+            <a href="http://levenia.com.ua">
+              <img src="/images/contests/levenia.png" alt="levenia" />
+            </a>
+            <a href="http://bober.net.ua">
+              <img
+                src="/images/contests/bebras.png"
+                alt="bebras"
+                className={styles.outlined}
+              />
+            </a>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <Link href="/">
+            <a href="/" className={styles.logo} aria-label="logo">
+              <Logo width={295 / 2} height={131 / 2} viewBox="0 0 295 131" />
             </a>
           </Link>
+          <Switcher />
         </div>
-        <Link href="/info/about/конкурси">
-          <div className={`${styles.label} ${styles.link}`}>Конкурси</div>
-        </Link>
-        <div className={styles.logos}>
-          <a href="http://kangaroo.com.ua">
-            <img src="/images/contests/kangaroo.png" alt="kangaroo" />
-          </a>
-          <a href="http://levenia.com.ua">
-            <img src="/images/contests/levenia.png" alt="levenia" />
-          </a>
-          <a href="http://bober.net.ua">
-            <img
-              src="/images/contests/bebras.png"
-              alt="bebras"
-              className={styles.outlined}
-            />
-          </a>
-        </div>
-        <Link href="/">
-          <a href="/" className={styles.logo} aria-label="logo">
-            <Logo width={295 / 2} height={131 / 2} viewBox="0 0 295 131" />
-          </a>
-        </Link>
-        <Switcher />
       </div>
     </div>
   );
